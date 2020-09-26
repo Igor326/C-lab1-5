3bbe8f29f01bb5393c6849f17a7b6c62af7861fc
Igor326 <egorkaluki6@gmail.com>
2020-09-26 21:03:20 +0300

    lab3

--- lab_1/lab2/Program.cs
+++ lab_1/lab2/Program.cs
@@ -5,8 +5,39 @@ namespace lab2
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+           double n=0;
+           double k=0;
+            double S=1;
+            int c = 0;
+            do
+            {
+                Console.WriteLine("Insert n value:(n>=0)");
+                 n = Convert.ToDouble(Console.ReadLine());
+                if (n < 0|| n%1!=0)
+                {
+                    Console.WriteLine("Wrong data was entered, try onother one");
+                    continue;
+                }
+                Console.WriteLine("Insert k value:(k>=n)");
+                k = Convert.ToDouble(Console.ReadLine());
+                if (k < n || k%1!=0)
+                {
+                    Console.WriteLine("Wrong data was entered, try onother one");
+                    continue;
+                }
+                break;
+            } while (true);
+            k = n * n;
+            n = n * k;
+            do
+            {
+                S = S* (Math.Pow(-1, k) * k * k + Math.Pow(-1, (k * k + 1)) * k) / (2 * k * k * k + 3);
+               
+                c++;
+            } while (k > c);
+            Console.WriteLine("Summary="+S);
         }
     }
 }
---
+++ lab_1/lab3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool answer;
+            int a;
+            int b;
+            int c;
+            while (true)
+            {
+                Console.WriteLine("Insert your value:(100-999)");
+                number= Convert.ToInt32(Console.ReadLine());
+                if (number<100 || number>999|| number%1!=0)
+                {
+                    Console.WriteLine("Wrong data was entered, try onother one");
+                    continue;
+                }
+                break;
+            }
+            c = number % 10;
+            number /= 10;
+            b = number % 10;
+            number /= 10;
+            a = number % 10;
+           
+            
+            
+            if (b > a && c > b)
+            {
+                answer = true;
+            }
+            else answer = false;
+            Console.WriteLine(answer);
+
+
+        }
+    }
+}
