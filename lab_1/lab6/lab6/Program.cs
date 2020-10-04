f985a1e3a9e59a17de09613b0c18179d459c4320
Igor326 <egorkaluki6@gmail.com>
2020-10-04 15:35:25 +0300

    lab6

---
+++ lab_1/lab6/lab6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab6
+{
+    class Program
+    {
+        static double Reverse(double a)
+        {
+            string s = a.ToString();
+            char[] ar = s.ToCharArray();
+            Array.Reverse(ar);
+            s = new string(ar);
+            a = Convert.ToInt32(s);
+            
+           
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            double t;
+            t = Convert.ToDouble(Console.ReadLine());
+           
+            Console.WriteLine(Reverse(t));
+        }
+    }
+}
