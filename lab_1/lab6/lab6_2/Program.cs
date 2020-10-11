40f720982da53b7741786d8c81aadb3e9e179b9e
Igor326 <egorkaluki6@gmail.com>
2020-10-11 17:46:21 +0300

    lab6_2

---
+++ lab_1/lab6/lab6_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab6_2
+{
+    class Program
+    {
+        static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Write something:");
+            str = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(ReverseString(str));
+
+        }
+    }
+}
