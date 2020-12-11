4e4829e75a454b5759fd9bfbb24c8fdaf91f0143
Igor326 <egorkaluki6@gmail.com>
2020-12-11 16:21:16 +0200

    lab 10 done

---
+++ lab10/lab10/Extentions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace lab10
+{
+    static class Extentions
+    {
+        public static int Reverse(this int value)
+        {
+            int temp = 0;
+            while (value > 0)
+            {
+                int remainded = value % 10;
+                temp = (temp * 10) + remainded;
+                value = value / 10;
+            }
+            return temp;
+        }
+        public static string Reverse(this string value)
+        {
+            string temp = string.Empty;
+            for (int i = value.Length - 1; i >= 0; i--)
+                temp += value[i];
+            return temp;
+        }
+        public static string SplitReverse(this string value)
+        {
+            string temp = string.Empty;
+            var strings = value.Split(',');
+
+            for (int i = 0; i < strings.Length - 1; i++)
+                temp += Reverse(strings[i]) + ',';
+
+            temp += Reverse(strings[strings.Length - 1]);
+
+            return temp;
+        }
+        public static double SplitReverse(this double value) =>
+           Double.Parse(SplitReverse(value.ToString(CultureInfo.CurrentCulture)));
+        public static T[] Reverse<T>(this T[] array)
+        {
+            T[] newArr = new T[array.Length];
+            for (int i = 0, j = array.Length - 1;
+                i < array.Length;
+                i++, j--)
+            {
+                newArr[i] = array[j];
+            }
+
+            return newArr;
+        }
+        public static void SortDecsOrderArr(this int[] array)
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            int task1 = 123;
+            Console.WriteLine("Task 1:");
+            Console.WriteLine(task1.Reverse());
+            // Task 2
+            string task2 = "test";
+            Console.WriteLine("Task 2:");
+            Console.WriteLine(task2.Reverse());
+            // Task 3
+            double task3 = 13.237;
+            Console.WriteLine("Task 3:");
+            Console.WriteLine(Convert.ToDouble(task3.SplitReverse()));
+            // Task 4
+            string task4 = "Hello, World!";
+            Console.WriteLine("Task 4:"  );
+            Console.WriteLine(task4.SplitReverse());
+            // Own variant(2) task
+            int[] variantTask = { 1, 23, 45, 0, -12, 77 };
+            Console.WriteLine("Task 5:");
+            variantTask.SortDecsOrderArr();
+        }
+    }
+}
