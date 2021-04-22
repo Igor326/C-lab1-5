4e0ef825ed93b5540c3b4232ab1588be25b6e12d
Igor326 <egorkaluki6@gmail.com>
2021-04-22 21:56:10 +0300

    lab7 3/5

---
+++ lab7_sem2/lab7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////1.
+            var directory = new DirectoryInfo(@"E:\LAB7");
+            if (directory.Exists)
+            {
+                Console.WriteLine("Директория существует.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Директория HE существует.");
+                Console.ReadKey();
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                string name = "Folder_" + i;
+                directory.CreateSubdirectory(name);
+
+                
+            }
+            Console.WriteLine("Папки создались.");
+            Console.ReadKey();
+            for (int i = 0; i < 100; i++)
+            {
+                string name = "Folder_" + i;
+                string fullName = "E:\\LAB7\\"+ name;
+                Directory.Delete(fullName, true);
+
+
+            }
+            Console.WriteLine("Папки удалились.");
+            Console.ReadKey();
+            
+            
+
+        }
+    }
+}
---
+++ lab7_sem2/lab7_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var directory = new DirectoryInfo(@"E:\LAB7");
+            string fullName="";
+
+            if (directory.Exists)
+            {
+                Console.WriteLine("Директория существует.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Директория HE существует.");
+                Console.ReadKey();
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                string name= "Folder_" + i+"\\";
+                fullName += name;
+ 
+            }
+            directory.CreateSubdirectory(fullName);
+            Console.WriteLine("Папки создались.");
+            Console.ReadKey();
+            Directory.Delete(@"E:\LAB7\Folder_0",true);
+            Console.WriteLine("Папки удалились.");
+            Console.ReadKey();
+            //Console.WriteLine(fullName);
+            //4.Максимальна довжина ім'я папки  - 247 символів
+
+        }
+    }
+}
