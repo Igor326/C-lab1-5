ee3871dcf379bd4568fc03f2c0c23f35f12db120
Igor326 <egorkaluki6@gmail.com>
2021-06-15 14:50:43 +0300

    added lab8sem2

---
+++ lab8sem2/lab8sem2/DataFromWindow.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace lab8sem2
+{
+    class DataFromWindow
+    {
+        public bool CheckBox1 { set; get; }
+        public bool CheckBox2 { set; get; }
+        public bool CheckBox3 { set; get; }
+        public string TextFieldContent { set; get; }
+        public double ScreenWidth { set; get; }
+        public double ScreenHeight { set; get; }
+    }
+}
---
+++ lab8sem2/lab8sem2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Text.Json;
+using System.IO;
+
+namespace lab8sem2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileName = @"E:\LAB7\test.";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void WriteDataButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DataFromWindow data = new DataFromWindow();
+            data.CheckBox1 = (bool)check1.IsChecked;
+            data.CheckBox2 = (bool)check2.IsChecked;
+            data.CheckBox3 = (bool)check3.IsChecked;
+
+            data.TextFieldContent = TextBoxContent.Text;
+
+            data.ScreenHeight = Height;
+            data.ScreenWidth = Width;
+
+            string json = JsonSerializer.Serialize(data);
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileName))
+                {
+                    string settings = JsonSerializer.Serialize(data);
+                    streamWriter.Write(settings);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Не удалось записать файл!");
+            }
+        }
+    }
+}
