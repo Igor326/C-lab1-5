adc5f0a92a28f5d670d8f5a0bdaba0f572b6eeed
Igor326 <egorkaluki6@gmail.com>
2021-06-15 15:27:55 +0300

    added individual work

---
+++ SamostiynaRobotaSem2/SamostiynaRobotaSem2/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamostiynaRobotaSem2
+{
+    public class Message
+    {
+        public User Initiator { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Data { get; set; }
+    }
+}
